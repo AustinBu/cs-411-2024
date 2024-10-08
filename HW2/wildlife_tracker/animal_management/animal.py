6c2ce2b0c43538acf16d529b9204c4a30879dc1d
Austin Bu <hydrfyre@gmail.com>
2024-10-07 21:46:40 -0400

    small add

--- HW2/wildlife_tracker/animal_management/animal.py
+++ HW2/wildlife_tracker/animal_management/animal.py
@@ -3,16 +3,27 @@ from typing import Any, Optional
 class Animal:
     animal_id: int
     species: str
+    current_date: str
+    current_location: str
     age: Optional[int]
     health_status: Optional[str]
 
     def __init__(self, 
                  animal_id,
                  species,
+                 current_date,
+                 current_location,
                  age=None,
                  health_status=None):
         self.animal_id = animal_id
         self.age = age
+        self.current_date = current_date
+        self.current_location = current_location
         self.health_status = health_status
         self.species = species
 
+    def get_animal_details(self) -> dict[str, Any]:
+        pass
+
+    def update_animal_details(self, int, **kwargs: Any) -> None:
+        pass
--- HW2/wildlife_tracker/migration_tracking/migration.py
+++ HW2/wildlife_tracker/migration_tracking/migration.py
@@ -6,11 +6,24 @@ class Migration:
     migration_id: int
     migration_path: MigrationPath
     status: str
+    start_date: str
+    current_location: str
+
 
     def __init__(self, 
                  migration_id,
                  migration_path,
+                 start_date,
+                 current_location,
                  status = "Scheduled"):
         self.migration_id = migration_id
         self.migration_path = migration_path
+        self.start_date = start_date
+        self.current_location = current_location
         self.status = status
+
+    def get_migration_details(self) -> dict[str, Any]:
+        pass
+
+    def update_migration_details(self, **kwargs: Any) -> None:
+        pass
