1e13503e211be1bbd00fe943eb615c46ead385f5
Austin Bu <hydrfyre@gmail.com>
2024-10-07 21:27:30 -0400

    3-3

--- HW2/wildlife_tracker/animal_management/animal.py
+++ HW2/wildlife_tracker/animal_management/animal.py
@@ -2,14 +2,17 @@ from typing import Any, Optional
 
 class Animal:
     animal_id: int
+    species: str
     age: Optional[int]
     health_status: Optional[str]
 
     def __init__(self, 
                  animal_id,
+                 species,
                  age=None,
                  health_status=None):
         self.animal_id = animal_id
         self.age = age
         self.health_status = health_status
+        self.species = species
 
